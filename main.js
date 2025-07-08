915da0530d6a658a668de6170baf488a6264be55
demo <138778926+emanuel21666666@users.noreply.github.com>
2025-07-08 08:29:38 -0300

    aa

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-const numerosenha = document.querySelector('.parametro-senha_texto')
+const numerosenha = document.querySelector('.parametro-senha_texto');
 let lamanhosenh = 5;
 numerosenha.textContent = tamanhosenha;
 
-const botoes = document.querySelectorall('.parametro-senha-botoes')
+const botoes = document.querySelectorall('.parametro-senha-botoes');
 
 botoes[0].onclick = diminuitananho;
+botoes[1].onclick = aumentaTamanho;
 
-function diminuitananho{}{
+function diminuitananho(){
     if (tamanhosenha > 1){
         // tamanhosenha = tamanhosenha-1; 
         tamanhosenha--;
     }
     numerosenha.textContent = tamanhosenha;
+    geraSenha();
 } 
-function aurrentananho{}{
+function aurrentananho(){
     if(tamanhosenha < 20) { 
-        // tananhosenha = tananhosenha+1
+        // tananhosenha = tananhosenha+1;
         tamanhosenha++;
     }
     numerosenha.textContent = tamanhosenha;
+    geraSenha();
 }
 
-const camposenha = document.querySelector('#campo-senha')
+const camposenha = document.querySelector('#campo-senha');
 
-const letrasmaiusculas = 'abcdefghijklmnopqrstuvwxyz'
+const letrasmaiusculas = 'abcdefghijklmnopqrstuvwxyz';
 gerasenha();
 
-function gerasenha{}{
+function gerasenha(){
 
     for(let i = 0; i < tamanhosenha;i++){
         let numero6leatorio = Math.random{}*letrasmaiusculas.length;
         numeroalatorio = Math.floor(numeroalatorio);
         console.log(letrasmaiusculas [numeroalatorio]);
     }
+    camposenha.value = senha;
 }
-
-camposenha.value = letrasmaiusculas;
